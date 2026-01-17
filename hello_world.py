ef7f52413030a9d8f6683086681f17fe1926797a
Dhiraj Paikrao <dhirajpaikrao4@gmail.com>
2026-01-17 21:31:17 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+import datetime
+#Taking user input
+name=input("Enter your name:")#taking name from user
+role=input("Enter your internship role:")#taking role from user
+todays_date=datetime.date.today()#taking date 
+
+#printing the output
+print("\nInternship Details")
+print("Name:",name)
+print("Role:",role)
+print("Date:",todays_date)
+
